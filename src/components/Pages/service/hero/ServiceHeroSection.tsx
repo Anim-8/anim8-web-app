44622b8ea92417fa135598e1ba95d4dcf981b816
spaulsteinberg <sampaulsteinberg@gmail.com>
2025-06-09 15:45:30 -0500

    use visual

--- src/components/Pages/service/hero/ServiceHeroSection.tsx
+++ src/components/Pages/service/hero/ServiceHeroSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LeadModal from '../../../leads/LeadModal';
 import useModal from '../../../../hooks/useModal';
 import ServiceHeroText from './ServiceHeroText';
-import ServiceHeroVisual from './ServiceHeroVisual';
+import ServiceVisual from '../ServiceVisual';
 
 const ServiceHeroSection: React.FC = () => {
   const { open, source, openModal, closeModal } = useModal();
@@ -10,7 +10,7 @@ const ServiceHeroSection: React.FC = () => {
   return (
     <div className="relative w-full flex flex-col lg:flex-row items-center justify-between gap-12 px-8 py-20">
       <ServiceHeroText openModal={openModal} />
-      <ServiceHeroVisual />
+      <ServiceVisual />
 
       {/* Detach modal from layout flow */}
       <LeadModal isOpen={open} onClose={closeModal} source={source} />
