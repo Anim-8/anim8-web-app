9f00ba3007bbfcd49298bb5cf1da53a9bab768a8
spaulsteinberg <sampaulsteinberg@gmail.com>
2025-06-12 21:24:35 -0500

    philosophy

--- src/models/common/Section.ts
+++ src/models/common/Section.ts
@@ -1,19 +1,29 @@
 import type { SectionVariant } from "../service/ServiceSection";
+import type { LabelValuePair } from "./LableValuePair";
 
 type CardItem =
-  | { title: string; description: string }
-  | { label: string; value: string }
-  | {title: string;  emoji: string; description: string;}
+    | { title: string; description: string }
+    | { label: string; value: string }
+    | { title: string; emoji: string; description: string; }
 
-interface BaseSection {
-  title: string;
-  subtitle: string;
-  description?: string;
-  variant: SectionVariant;
-  modalText?: string;
-  primaryTitleColor?: string;
-  items: CardItem[];
-  visualSlot?: React.ReactNode
+interface T {
+    title: string;
+    visualSlot?: React.ReactNode
+    description?: string;
 }
 
-export type { CardItem, BaseSection }
+interface BaseSection extends T {
+    subtitle: string;
+    variant: SectionVariant;
+    modalText?: string;
+    primaryTitleColor?: string;
+    items: CardItem[];
+}
+
+interface PhilosophySection extends T {
+    items: LabelValuePair[];
+    footer?: string
+    itemTitle?: string
+}
+
+export type { CardItem, BaseSection, PhilosophySection }
