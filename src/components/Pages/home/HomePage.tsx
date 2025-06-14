9d5766f5c6f7d193c8309ad3ac49178a18dbbb3b
i-anim8 <roberto.calvi@anim8.tech>
2025-06-14 11:50:54 -0400

    home page mobile responsive

--- src/components/Pages/home/HomePage.tsx
+++ src/components/Pages/home/HomePage.tsx
@@ -6,21 +6,22 @@ import ServiceHero from './ServiceHero';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="h-screen w-full overflow-y-scroll scroll-smooth snap-y snap-mandatory scrollwidth">
-      <section id="hero" className="h-screen snap-start">
+    <div className="w-full overflow-y-scroll scroll-smooth scrollwidth snap-none md:snap-y md:snap-mandatory">
+      <section id="hero" className="h-auto md:h-screen snap-start">
         <HeroSection />
       </section>
-      <section id="hero" className="h-screen snap-start">
+      <section id="product" className="h-auto md:h-screen snap-start">
         <ProductHeroSection />
       </section>
-      <section id="hero" className="h-screen snap-start">
+      <section id="service" className="h-auto md:h-screen snap-start">
         <ServiceHero />
       </section>
-      <section id="hero" className="h-screen snap-start">
+      <section id="philosophy" className="h-auto md:h-screen snap-start">
         <PhilosophyHeroSection />
       </section>
     </div>
   );
 };
+
 
 export default HomePage;
