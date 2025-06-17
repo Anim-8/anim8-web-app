1c565fecd3114b202a2beff14207b78deb3e563e
i-anim8 <roberto.calvi@anim8.tech>
2025-06-17 08:49:15 -0400

    changing base path again

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/anim8-web-app/', // Your repo name
   plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
 })
