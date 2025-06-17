0d96f24b39337b49ca834d4c560c65669848ab38
spaulsteinberg <sampaulsteinberg@gmail.com>
2025-06-17 12:14:11 -0500

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/anim8-web-app/",
   plugins: [react(), tailwindcss()],
   build: {
     sourcemap: false,
