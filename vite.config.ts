e22a84d5865c4a84e05b9ab331c2948d7300c94c
i-anim8 <roberto.calvi@anim8.tech>
2025-06-16 20:03:55 -0400

    Fix deployement errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/anim8-web-app/', 
+  base: '/', 
   plugins: [react(), tailwindcss()],
 })
