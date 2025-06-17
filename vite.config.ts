d5fe26674b4b72696f5399f04c70687ca0ca9f4a
spaulsteinberg <sampaulsteinberg@gmail.com>
2025-06-17 12:13:12 -0500

    test base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/anim8-web-app/",
   plugins: [react(), tailwindcss()],
   build: {
     sourcemap: false,
