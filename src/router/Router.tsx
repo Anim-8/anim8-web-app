d3569ed7ef4f141ff31f24616a72ea3c03cc0d4d
i-anim8 <roberto.calvi@anim8.tech>
2025-10-08 14:29:56 -0400

    fixing imports in Router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,22 +1,20 @@
-import { createRoutesFromElements, Route, RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "../components/pages_1/home/HomePage.tsx";
-
+import React from 'react';
+import { createRoutesFromElements, Route, RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router";
+import HomePage from "../components/pages_1/home/HomePage";
 import RootLayout from "./layouts/RootLayout";
 import NotFound from "./layouts/NotFound";
-import ProductPage from "../components/pages_1/product/ProductPage.tsx";
-import ServicePage from "../components/pages_1/service/ServicePage.tsx";
-import PhilosophyPage from "../components/pages_1/philosophy/PhilosophyPage.tsx";
-import PistRampSimulator from "../components/simulator/PistRampSimulator.tsx";
-
+import BlogListingPage from "../pages/blog/BlogListingPage";
+import ArticlePage from "../pages/blog/ArticlePage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<RootLayout />}>
             <Route index element={<HomePage />} />
-            <Route path="product" element={<ProductPage />} />
-            <Route path="service" element={<ServicePage />} />
-            <Route path="philosophy" element={<PhilosophyPage />} />
-            <Route path="simulators" element={<PistRampSimulator/>} />
+            <Route path="blog">
+                <Route index element={<BlogListingPage />} />
+                <Route path=":slug" element={<ArticlePage />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
         </Route>
     )
